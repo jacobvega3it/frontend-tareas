4f097f07cea998e0fe5a883e9c065a44be96e411
Jacob Vega <jacob.vega@3it.cl>
2025-01-15 16:18:31 -0300

    Primera versión del proyecto

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Lista de Tareas</h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Nueva tarea" />
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task }}
+        <button @click="removeTask(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask){
+        console.log(this.newTask);
+        this.tasks.push(this.newTask);
+        console.log(this.tasks);
+        this.newTask = "";
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    }
+  }
+};
+</script>
