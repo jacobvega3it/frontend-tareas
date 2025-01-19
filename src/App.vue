fc7d3ff61737c787028128f9c603a55588ccbc3b
Jacob Vega <jacob.vega@3it.cl>
2025-01-19 20:44:30 -0300

    Se agrega componente de PropiedadComputada

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <ListadoTareas />
     <ContadorReactivo />
+    <PropiedadComputada />
   </div>
 </template>
 
 <script>
 import ContadorReactivo from './components/ContadorReactivo.vue';
 import ListadoTareas from './components/ListadoTareas.vue';
+import PropiedadComputada from './components/PropiedadComputada.vue';
 
 export default {
   components: {
     ContadorReactivo, 
-    ListadoTareas
+    ListadoTareas,
+    PropiedadComputada
   }
 };
 </script>
---
+++ src/components/PropiedadComputada.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Propiedad Computada</h1>
+        <p>Ingrese un número: <input v-model="numero" /></p>
+        <p>El número ingresado es: {{ numero }}</p>
+        <p>El número incrementado en 10 es: {{ numeroIncrementado }}</p>
+    </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+
+export default {
+    
+    setup() {
+        const numero = ref(0);
+
+        const numeroIncrementado = computed(() => {
+            return parseInt(numero.value) + 10;
+        });
+
+        return { numero, numeroIncrementado };
+    }
+};
+</script>
